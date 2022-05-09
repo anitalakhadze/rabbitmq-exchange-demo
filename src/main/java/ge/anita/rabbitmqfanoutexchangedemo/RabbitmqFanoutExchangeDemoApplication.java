daf16573c77d962d75fd758fd87d91bf68cef007
atala15 <atala15@freeuni.edu.ge>
2022-05-09 21:40:37 +0400

    Initial commit

---
+++ src/main/java/ge/anita/rabbitmqfanoutexchangedemo/RabbitmqFanoutExchangeDemoApplication.java
@@ -0,0 +1,13 @@
+package ge.anita.rabbitmqfanoutexchangedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqFanoutExchangeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqFanoutExchangeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/anita/rabbitmqfanoutexchangedemo/RabbitmqFanoutExchangeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.anita.rabbitmqfanoutexchangedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqFanoutExchangeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
