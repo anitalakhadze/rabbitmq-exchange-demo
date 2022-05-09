696a13a0a15cee7503079b895482940dc0c7150f
atala15 <atala15@freeuni.edu.ge>
2022-05-09 22:42:19 +0400

    Main class of the application.

--- src/main/java/ge/anita/rabbitmqexchangedemo/RabbitmqExchangeDemoApplication.java
+++ src/main/java/ge/anita/rabbitmqexchangedemo/RabbitmqExchangeDemoApplication.java
@@ -1,13 +1,13 @@
-package ge.anita.rabbitmqfanoutexchangedemo;
+package ge.anita.rabbitmqexchangedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqFanoutExchangeDemoApplication {
+public class RabbitmqExchangeDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitmqFanoutExchangeDemoApplication.class, args);
+        SpringApplication.run(RabbitmqExchangeDemoApplication.class, args);
     }
 
 }
